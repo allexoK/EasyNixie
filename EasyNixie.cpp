664b5e54f12ce6a62f8f7e684ecac0d1ea106d28
allexok <alexok997@gmail.com>
2023-09-23 16:29:29 +0200

    dimming support added

--- EasyNixie.cpp
+++ EasyNixie.cpp
@@ -12,7 +12,10 @@ EasyNixie::EasyNixie(uint8_t outputEnable,uint8_t shcp, uint8_t stcp, uint8_t ds
     pinMode(dsin, OUTPUT);
 }
 
-void EasyNixie::SetNixie(uint8_t number,uint8_t color,bool voltage,bool comma){
+void EasyNixie::SetNixie(uint8_t number,uint8_t color,bool voltage,bool comma,uint16_t dimming){
+    #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)//AnalogWrite is supported for some platfroms and not supported for others(Esp32?), so if you want to used dimming in this platoforms you can implement it ourself
+    analogWrite(outputEnable,dimming);
+    #endif
     uint8_t secondShiftRegisterData=0b00011100;
     if(number==8)secondShiftRegisterData|=0b00000001;
     if(number==9)secondShiftRegisterData|=0b00000010;
--- examples/Hello_Nixie/Hello_Nixie.ino
+++ examples/Hello_Nixie/Hello_Nixie.ino
@@ -1,3 +1,4 @@
+#include "/home/alex/Documents/personalProjects/boards/EasyNixie-master/EasyNixie.cpp"
 #define OUT_EN 3   //connect Pin ArduinoUNO 3 to OUT_EN
 #define SHCP   2   //connect Pin ArduinoUNO 2 to SHCP
 #define STCP   6   //connect Pin ArduinoUNO 6 to STCP
@@ -13,12 +14,12 @@ void loop()                          //Writing 0-9 number Value
 {
   for (uint8_t i = 0; i <= 9; i++)      
   {                                  
-    en.SetNixie(i,5,true,true);           //en.SetNixie(NixieNubmer , Color , ON/Off_Nixie);   Set color= 5= Purple
+    en.SetNixie(i,5,true,true,100);           //en.SetNixie(NixieNubmer , Color , ON/Off_Nixie);   Set color= 5= Purple
     en.Latch();                      //Send your set to EaseNixie 
 
     Serial.print("'i' is equal: ");  //Print to COM actual Value
     Serial.println(i);
-    delay(1400);                     //Wait 1,4sec  
+    delay(1000);                     //Wait 1,4sec  
   }  
   
 }
